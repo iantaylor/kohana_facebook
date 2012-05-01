3679239a9121e185be1674aa87379b504f33e51d
Ian Taylor <ian@ian-taylor.ca>
2012-04-30 23:54:10 -0300

    Factory method and basic FB set up

--- classes/kohana/facebook.php
+++ classes/kohana/facebook.php
@@ -4,11 +4,25 @@
 class Kohana_Facebook
 {
 
-	protected function __construct()
+	protected $_facebook; 
+
+	public function __construct()
 	{	
-		//Include the required facevook vendor files
-		include Kohana::find_file('vendor', 'facebook/src/facebook');
+		//Include the required facebook vendor files
+		include Kohana::find_file('vendor', 'facebook-sdk/src/facebook');
+
+
+		$this->_facebook = new Facebook(array(
+				'appId'		=> Kohana::$config->load('facebook.app_id'),
+				'secret'	=>	Kohana::$config->load('facebook.secret')
+		));
 
 	}	
+
+
+	public static function factory()
+	{
+		return new kohana_facebook ();
+	}
 
 }
